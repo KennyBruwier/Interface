21e5153407dff4c4d20ea26adcab6aea29b9233a
Kenny Bruwier <kenny.bruwier@gmail.com>
2021-03-22 16:07:55 +0100

    Kleine aanpassing

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -12,13 +12,15 @@ namespace Interface
      * Wat is een interface?
      * 
      * Kort:
-     *  Classes kunnen worden verbonden aan elkaar door ze te koppelen met een interface.
-     *  Met een interface zeg je welke methodes de gelinkte classes moeten hebben. 
+     *  Klassen kunnen worden verbonden aan elkaar door ze te koppelen met een interface.
+     *  Met een interface zeg je welke methodens de gelinkte klassens moeten hebben. 
      *  Die interface gebruik je dan als nieuw datatype om zo toegang te krijgen tot 
-     *  de methodes en structures die je verplicht hebt.
-     *  Dus i.p.v. een parent classe te maken, ga je een interface gebruiken.
-     *  Alsof je een toetsenbord maakt waarbij de verschillende toetsen de verplichte methodens voorstellen.
-     *  Wanneer je één toets indrukt, voer je de methodes uit binnen al die sub classens. 
+     *  de methodes van de onderliggende klassens.
+     *  
+     *  Alsof je een toetsenbord maakt waarbij de toetsen gekoppeld zijn aan een methoden 
+     *  binnen al uw gelinkte klassens. Wanneer je één toets indrukt, voer je die methoden 
+     *  uit binnen al die sub klassens. Zo kan je met uw "interface-toetsbord" 
+     *  verschillende methodens tegelijk laten uitvoeren binnen al uw gelinkte klassens.
      */
 
     class Program
